2dc37fa383348973560add92c6c26244ff51b467
Ian <iandkaneshiro@gmail.com>
2024-01-25 09:29:44 -0800

    created basic app functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,30 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+
+const tipbits = [
+  "Lets Pomodora!!!!",
+  "Ehhh I think well be lazy slugs today",
+  "Hard work is best work",
+  "Its the litset when your working",
+  "Focus is the best",
+  "Naps > working all day",
+  "Where are my kitties...",
+];
 
 function App() {
-  const [count, setCount] = useState(0);
-
+  const [index, setIndex] = useState(null);
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>Hocus Pocus! Lets Focus!</h1>
+      <h2>{tipbits[index]}</h2>
+      <p>Let see what we are doing today!</p>
+      <button
+        onClick={() => {
+          setIndex(Math.floor(Math.random() * tipbits.length));
+        }}
+      >
+        Click Me
+      </button>
+    </div>
   );
 }
 
