20828174989a6c3452fcf5e49e59658c418eda84
Ian <iandkaneshiro@gmail.com>
2024-01-29 19:12:35 -0800

    timer rough funtionallity added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,34 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [time, setTime] = useState(0);
+  const [timerId, setTimerId] = useState(null);
+  const [timeValue, setTimeValue] = useState(0);
+
+  const startTimer = () => {
+    if (!timerId) {
+      setTimerId(
+        setInterval(() => {
+          setTime((time) => time - 1);
+        }, 1000)
+      );
+    }
+  };
+
+  const stopTimer = () => {
+    clearInterval(timerId);
+    if (timerId) setTimerId(null);
+  };
+
   return (
     <>
       <main className="main">
         <div className="main_timer">
           <h1>WORK</h1>
-          <h2>00:25:00</h2>
+          <h2>{`${Math.floor(time / 3600)}:${Math.floor((time % 3600) / 60)}:${
+            time % 60
+          }`}</h2>
         </div>
         <div>
           <img src="images.png" alt="hourglass" />
@@ -14,8 +36,14 @@ function App() {
         <div className="main_progress-bar"></div>
       </main>
       <div className="main_timer-options">
-        <button>New Timer</button>
-        <button>Hide Timer</button>
+        <input
+          type="number"
+          placeholder={time}
+          onChange={(e) => setTimeValue(e.target.value)}
+        />
+        <button onClick={() => setTime(timeValue)}>Set Time</button>
+        <button onClick={startTimer}>Start Timer</button>
+        <button onClick={stopTimer}>Stop Timer</button>
       </div>
     </>
   );
