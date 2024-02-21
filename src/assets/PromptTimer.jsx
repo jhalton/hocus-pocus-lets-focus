213ce453809130d2d947939cf19155546684122a
Jeanette <haltonjeanette@gmail.com>
2024-02-20 16:53:00 -0800

    The countdown counts down, but it's decrementing the wrong thing at the wrong time

--- src/assets/PromptTimer.jsx
+++ src/assets/PromptTimer.jsx
@@ -5,12 +5,58 @@ const PromptTimer = () => {
   const [hours, setHours] = useState(0);
   const [minutes, setMinutes] = useState(0);
   const [seconds, setSeconds] = useState(0);
+  const [intervalId, setIntervalId] = useState(null);
+  const [isPaused, setIsPaused] = useState(true);
 
   useEffect(() => {
     //TODO
     //I want to count down each second. When the seconds run to 0,
     //we need to decrement the minutes, then start the seconds over
-  });
+
+    return () => clearInterval(intervalId);
+  }, intervalId);
+
+  const handleClearTimer = () => {
+    setHours(0);
+    setMinutes(0);
+    setSeconds(0);
+
+    clearInterval(intervalId);
+    setIsPaused(true);
+  };
+
+  const handleStartTimer = () => {
+    if (isPaused) {
+      const id = setInterval(() => {
+        if (seconds > 0) {
+          setSeconds((prevSeconds) => prevSeconds - 1);
+        } else {
+          if (minutes > 0) {
+            setMinutes((prevMinutes) => prevMinutes - 1);
+            setSeconds(59);
+          } else {
+            if (hours > 0) {
+              setHours((prevHours) => prevHours - 1);
+              setMinutes(59);
+              setSeconds(59);
+            } else {
+              clearInterval(id);
+              setIsPaused(true);
+            }
+          }
+        }
+      }, 1000);
+      setIntervalId(id);
+      setIsPaused(false);
+    }
+  };
+
+  const handleStopTimer = () => {
+    //TODO
+    //Pause timer
+    clearInterval(intervalId);
+    setIsPaused(true);
+  };
 
   return (
     <>
@@ -21,11 +67,10 @@ const PromptTimer = () => {
         </span>
         <img src={stopwatch} alt="stopwatch logo" />
         <div className="prompt-timer--buttons">
-          <button>Clear Timer</button>
+          <button onClick={handleClearTimer}>Clear Timer</button>
           <button>Set Timer</button>
-
-          <button>Start Timer</button>
-          <button>Stop Timer</button>
+          <button onClick={handleStartTimer}>Start Timer</button>
+          <button onClick={handleStopTimer}>Stop Timer</button>
           <form>
             <label>Hours: </label>
             <input
